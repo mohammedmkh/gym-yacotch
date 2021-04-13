c4d4d3db5ae3d9978db79e746617105d520d5265
mohammedmkh <mohammed91my@gmail.com>
2021-04-13 10:43:03 +0300

    new assets

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,20 +13,44 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Page Vendors Styles(used by this page)-->
-    <link href="{{url('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet" type="text/css" />
-    <!--end::Page Vendors Styles-->
-    <!--begin::Global Theme Styles(used by all pages)-->
-    <link href="{{url('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{url('assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{url('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
 
-
-    <link href="{{url('assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
 
 
     <!--begin::Layout Themes(used by all pages)-->
     <!--end::Layout Themes-->
     <link rel="shortcut icon" href="{{url('assets/media/logos/favicon.ico')}}" />
+
+    @if(session()->get('language') == 'en')
+
+        <link href="{{url('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
+
+
+    @else
+
+
+        <link href="{{url('assets/plugins/custom/fullcalendar/fullcalendar.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/plugins/global/plugins.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/plugins/custom/prismjs/prismjs.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/css/style.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/css/themes/layout/header/base/light.rtl.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/css/themes/layout/header/menu/light.rtl.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/css/themes/layout/brand/dark.rtl.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{url('assets/css/themes/layout/aside/dark.rtl.css')}}" rel="stylesheet" type="text/css" />
+
+
+        <link href="https://fonts.googleapis.com/css?family=Cairo&display=swap" rel="stylesheet">
+        <style>
+            *, h2,h1,h3,h3,p{
+                font-family: 'Cairo', sans-serif;
+            }
+
+        </style>
+
+    @endif
 
     @toastr_css
 </head>
